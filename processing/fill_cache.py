5125ccc7781439f90725f132353358c75a498309
Daniel Ferreira <dcferreira@users.noreply.github.com>
2017-03-17 12:33:33 +0100

    added structures for all types of Entities in the API; cleaned up paths; introduced config file

---
+++ processing/fill_cache.py
@@ -0,0 +1,12 @@
+from parse import *
+from conf import PROJECT_PATH
+
+
+PAPERS_DIR = PROJECT_PATH + '/papers'
+
+
+goals = list_goals(PAPERS_DIR)
+print(goals)
+
+for pap in list_references(PAPERS_DIR):
+    print(pap.title, pap.citations)
--- processing/parse.py
+++ processing/parse.py
@@ -37,9 +37,10 @@ def _open_json(filename):
 
 
 def _iterate_directory(directory):
-    for filename in sorted(os.listdir(directory)):
-        if filename[-5:] == '.json':
-            yield _open_json(directory + os.sep + filename)
+    for year in sorted(os.listdir(directory)):
+        for filename in sorted(os.listdir(directory + os.sep + year)):
+            if filename[-5:] == '.json':
+                yield _open_json(directory + os.sep + year + os.sep + filename)
 
 
 ##############################################
@@ -120,17 +121,17 @@ def list_operations(directory):
 
 
 def list_references(directory):
-    out = set()
+    out = []
     for d in _iterate_directory(directory):
-        out.add(Reference(d))
+        out.append(Reference(d))
     return out
 
 
 def list_flow_references(directory):
-    out = set()
+    out = []
     for d in _iterate_directory(directory):
         if d['flows'] is not None and len(d['flows']) > 0:
-            out.add(Reference(d))
+            out.append(Reference(d))
     return out
 
 
