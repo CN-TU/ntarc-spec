699ec20f4c169ed167efd34626b35b058107c5ab
Daniel Ferreira <dcferreira@users.noreply.github.com>
2017-04-27 11:37:43 +0200

    updated verify script

--- verify.sh
+++ verify.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-cd processing && python scripts/verify.py ../"$@"
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd`
+popd > /dev/null
+
+hash jsonschema 2>/dev/null || { echo >&2 "Please intall jsonschema: \`pip install jsonschema\`"; exit 1; }
+if [ -z "$1" ]
+then
+    echo "Usage: ./verify.sh FILENAME_TO_TEST.json"
+    exit 1
+fi
+jsonschema -i $1 $SCRIPTPATH/schema_v2.json && echo "No errors!"
