0584fa5ff501d9451aa552b8442cd4d5f8c1d4a1
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-02-15 14:19:32 +0100

    implemented budget for down-leveling

--- v2_processing/structures/blocks/preprocessing/features.py
+++ v2_processing/structures/blocks/preprocessing/features.py
@@ -19,12 +19,38 @@ class FeatureError(Exception):
         self.message = message
 
 
+def _get_iana_ies(filename):
+    out = set()
+    with open(filename, 'rb') as fd:
+        fd.readline()  # skip headers
+        text = fd.read().split(b'\r\n')
+        for line in text:
+            line = line.decode()
+            if len(line) > 0:
+                out.add(line.split(',')[1])
+    return out
+
+
+def _get_own_ies(filename):
+    out = set()
+    with open(filename) as fd:
+        for line in fd:
+            out.add(line.split(',')[0])
+    return out
+
+
+def _get_aliases(filename):
+    with open(filename) as fd:
+        d = json.load(fd)
+    return d
+
+
 def get_rules(target):
     out = []
     match_line = re.compile('^<'+target+'> -> ')
     for line in specification:
         if match_line.match(line):  # check if line has rule for target
-            options = [x.strip() for x in line.split('->')[-1].split('#')[0].split('|')]
+            options = [x.strip() for x in line.split('->')[1].split('#')[0].split('|')]
             out.extend(options)
     return out
 
@@ -44,7 +70,7 @@ def get_operations(rules):
 
 def get_non_term_symbols(rules):
     out = []
-    symb_regex = re.compile('^\s*\<[\w-]+\>\s*$')
+    symb_regex = re.compile('^\s*\<[\w0-9-]+\>\s*$')
     for rule in rules:
         if symb_regex.match(rule):
             out.append(rule)
@@ -56,13 +82,15 @@ def get_term_symbols(rules):
     symb_regex = re.compile('^\s*"\w+"\s*$')
     non_quoted_regex = re.compile('^\w*$')
     for rule in rules:
-        if symb_regex.match(rule) or non_quoted_regex.match(rule):
-            out.append(rule)
+        if symb_regex.match(rule):
+            out.append(rule.strip('"'))
+        elif non_quoted_regex.match(rule):
+            out.append(json.loads(rule))
     return out
 
 
-def match_arguments(op_args, input_args):
-    if len(op_args) != len(input_args) and ('+' not in op_args and '*' not in op_args):
+def match_arguments(op_args, input_args, level):
+    if len(op_args) != len(input_args) and ('+' not in str(op_args) and '*' not in str(op_args)):
         raise FeatureError(input_args, op_args, 'Wrong size!')  # check for basic input structure
     out = []
     idx = 0
@@ -72,7 +100,7 @@ def match_arguments(op_args, input_args):
             parsed_args = []
             while idx < len(input_args):
                 try:
-                    parsed_args.append(parse_as(input_args[idx], arg[:-1]))
+                    parsed_args.append(parse_as(input_args[idx], arg[:-1], level))
                     idx += 1
                 except FeatureError:
                     break
@@ -80,7 +108,7 @@ def match_arguments(op_args, input_args):
                 raise FeatureError(input_args[idx:], arg, 'Need to consume at least one argument (consumed 0)!')
             out.extend(parsed_args)
         else:
-            out.append(parse_as(input_args[idx], arg))
+            out.append(parse_as(input_args[idx], arg, level))
             idx += 1
     if len(out) != len(input_args):
         raise FeatureError(input_args, op_args, 'Not all arguments were consumed!')
@@ -88,8 +116,8 @@ def match_arguments(op_args, input_args):
     return out
 
 
-def parse_as(argument, name):
-    return possible_types[name.strip('<>')](argument)
+def parse_as(argument, name, level):
+    return possible_types[name.strip('<>')](argument, level)
 
 
 class Base(object):
@@ -99,20 +127,37 @@ class Base(object):
     non_term = None
     term = None
     leaf = False
+    aliases = _get_aliases(CONVERSION_FILE)
 
-    def __init__(self, d):
+    def __init__(self, d, level):
+        self.level = level
+        if self.name in ['down', 'down2']:
+            self.level -= 1
+        if self.level < 0:
+            raise FeatureError(d, self.name, "Already at bottom level, can't go further down.")
+
+        if type(d) == str and d in self.aliases:
+            d = self.aliases[d]
+
         self.key = None
         self.arguments = None
         self.possible_parses = []
         self.basefeat = None
+        self.errors = []
 
         self.parse(d)
 
     def parse(self, d):
         try:
             self.parse_as_self(d)
-        except FeatureError:
-            self.parse_non_terminal(d)
+        except FeatureError as e:
+            self.errors.append(e)
+            try:
+                self.parse_non_terminal(d)
+            except FeatureError as e:
+                self.errors.append(e)
+        if len(self.possible_parses) == 0:
+            raise FeatureError(d, self.name, self.errors)
 
     def parse_as_self(self, d):
         # op
@@ -127,9 +172,10 @@ class Base(object):
             # validate arguments
             for op in self.ops[self.key]:
                 try:
-                    args = match_arguments(op, self.arguments)
+                    args = match_arguments(op, self.arguments, self.level)
                     self.possible_parses.append(args)
-                except FeatureError:
+                except FeatureError as e:
+                    self.errors.append(e)
                     continue
             if len(self.possible_parses) == 0:
                 raise FeatureError(d, self.name, "Can't parse directly!")
@@ -149,9 +195,10 @@ class Base(object):
         # non-terminal symbol
         for symbol in self.non_term:
             try:
-                arg = parse_as(d, symbol)
+                arg = parse_as(d, symbol, self.level)
                 self.possible_parses.append(arg)
             except FeatureError as e:
+                self.errors.append(e)
                 continue
         if len(self.possible_parses) == 0:
             raise FeatureError(d, self.name, "Doesn't match any non-terminal symbol.")
@@ -159,8 +206,8 @@ class Base(object):
     def iterate_operations(self):
         if self.key is not None:
             yield self.key
-        elif not self.leaf:
-            if type(self.possible_parses[0]) == list: # TODO continue
+        if not self.leaf:
+            if type(self.possible_parses[0]) == list:
                 for arg in self.possible_parses[0]:
                     for op in arg.iterate_operations():
                         yield op
@@ -200,45 +247,18 @@ def get_type(inp_name):
     return new_class
 
 
-def _get_iana_ies(filename):
-    out = set()
-    with open(filename, 'rb') as fd:
-        fd.readline()  # skip headers
-        text = fd.read().split(b'\r\n')
-        for line in text:
-            line = line.decode()
-            if len(line) > 0:
-                out.add(line.split(',')[1])
-    return out
-
-
-def _get_own_ies(filename):
-    out = set()
-    with open(filename) as fd:
-        for line in fd:
-            out.add(line.split(',')[0])
-    return out
-
-
-def _get_aliases(filename):
-    with open(filename) as fd:
-        d = json.load(fd)
-        out = set(list(d.keys()))
-    return out
-
 class BaseFeature(Base):
     name = 'base-feature'
     iana_ies = _get_iana_ies(IANA_IE_LIST)
     own_ies = _get_own_ies(OWN_IE_LIST)
-    aliases = _get_aliases(CONVERSION_FILE)
-    rare_feature = re.compile('^__[a-z0-9]+([A-Z][a-z0-9]*)+$')
+    # aliases = _get_aliases(CONVERSION_FILE)
+    rare_feature = re.compile('^__[a-z0-9]+([A-Z][a-z0-9]*)*$')
     leaf = True
 
     def parse(self, d):
         if type(d) != str:
             raise FeatureError(d, self.name, 'Base features need to be strings!')
         if not self.rare_feature.match(d) and \
-                        d not in self.aliases and \
                         d not in self.own_ies and \
                         d not in self.iana_ies:
             raise FeatureError(d, self.name, 'Base feature doesn\'t exist or has invalid name!')
@@ -254,15 +274,15 @@ class Number(Base):
             raise FeatureError(d, self.name, 'Not a number!')
 
 
-possible_types = {k: get_type(k) for k in ['value', 'values', 'selection', 'feature', 'logic']}
+possible_types = {k: get_type(k) for k in ['value', 'values', 'selection', 'down', 'logic', 'logic-down', 'down2']}
 possible_types['base-feature'] = BaseFeature
 possible_types['free-integer'] = Number
 possible_types['free-float'] = Number
 
 
 class Features(object):
-    def __init__(self, d):
-        self.features = [possible_types['feature'](f) for f in d]
+    def __init__(self, d, level):
+        self.features = [possible_types['value'](f, level) for f in d]
 
     def iterate_features(self):
         return self.iterate_base_features()
@@ -284,27 +304,76 @@ class Features(object):
             for ff in f.iterate_base_features():
                 yield ff
 
-# # TODO tmp
-# if __name__ == '__main__':
-#     example = {
-#             "count": [
-#               {
-#                 "quantile_range": [
-#                   "_flowStartHours",
-#                   0,
-#                   0.05
-#                 ]
-#               }
-#             ]
-#           }
-#     # [print(x) for x in get_rules('value')]
-#     # [print(x) for x in get_rules('feature')]
-#     # print(get_operations(get_rules('value'))['add'])
-#     vv = possible_types['feature']
-#     i = vv(example)
-#     f = Features([example])
-#     print(f.get_base_feature_names())
-#     print(f.get_operation_names())
-#     # print(i)
-#
-#     # Value = type(ValueMeta)
+
+if __name__ == '__main__':
+    # some features that should work
+    packet_features = [
+        "octetTotalCount"
+    ]
+    flow_features = [
+        "_flowDurationSeconds",
+        # total octetTotalCount
+        "octetTotalCount",
+        # mean of octetTotalCount in packets
+        {"mean": ["octetTotalCount"]},
+        # mean of octetTotalCount in packets that have less than 1000 octetTotalCount
+        {"mean": [{"map": ["octetTotalCount",
+                           {"select": [{"less": ["octetTotalCount", 1000]}]}]}]},
+        {
+            "count": [
+              {
+                "select": [
+                  {
+                    "equal": [
+                      "ipClassOfService",
+                      {
+                        "mode": [
+                          "ipClassOfService"
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+    ]
+    flowagg_features = [
+        "_flowDurationSeconds",
+        # total octetTotalCount
+        "octetTotalCount",
+        # mean of octetTotalCount in flows
+        {"mean": ["octetTotalCount"]},
+        # mean of octetTotalCount in flows with less than 1000 octetTotalCount
+        {"mean": [{"map": ["octetTotalCount",
+                           {"select": [{"less": ["octetTotalCount", 1000]}]}]}]},
+        # mean (over flows) of mean (over packets) of octetTotalCount (using map twice)
+        {"mean": [{"map": [
+            {"mean": [{"map": ["octetTotalCount", {"select": [True]}]}]},
+            {"select": [True]}
+        ]}]},
+        # mean (over flows) of mean (over packets) of octetTotalCount
+        {"mean": [{"map": [
+            {"mean": ["octetTotalCount"]},
+            {"select": [True]}
+        ]}]},
+        # mean of octetTotalCount for all packets
+        {"mean": [{"flat_map": [
+            "octetTotalCount",
+            {"select": [True]}
+        ]}]},
+        # mean of octetTotalCount for packets that have less than 1000 octetTotalCount and are in flows with less than
+        #  30 packets
+        {"mean": [{"flat_map": [
+            # get octetTotalCount
+            "octetTotalCount",
+            # of packets in flows with less than 30 packets
+            {"select": [{"greater": ["packetTotalCount", 30]}]},
+            # only counting packets with less than 1000 octetTotalCount
+            {"select": [{"less": ["octetTotalCount", 1000]}]}
+        ]}]}
+    ]
+
+    Features(packet_features, 0)
+    Features(flow_features, 1)
+    Features(flowagg_features, 2)
