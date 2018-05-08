fb93190b7f618f28638328f377a17b0bf1212342
Daniel Ferreira <dcferreira@users.noreply.github.com>
2018-05-08 12:36:37 +0200

    updated tool name attribute name

--- v2_processing/structures/tool.py
+++ v2_processing/structures/tool.py
@@ -1,5 +1,5 @@
 class Tool(object):
     def __init__(self, obj):
-        self.tool = obj['name']
+        self.name = obj['name']
         self.detail = obj['detail'] if 'detail' in obj else None
         self.availability = obj['availability']
